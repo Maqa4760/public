0ec823cd9672ac4c181d0af925cae170825049ed
Maqa4760 <mabasov29@gmail.com>
2024-12-27 22:34:48 +0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('category-button').addEventListener('click', function() {
-    document.getElementById('category-window').classList.toggle('hidden');
-});
-
-window.addEventListener('click', function(event) {
-    if (!event.target.closest('#category-button') && !event.target.closest('#category-window')) {
-        document.getElementById('category-window').classList.add('hidden');
-    }
-});
